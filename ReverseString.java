925f501b189e837302b67d0bdf3ef6dc2ab63a22
Sezan-Hasibur <60995411+Sezan-Hasibur@users.noreply.github.com>
2020-02-17 10:17:39 +0600

    Add files via upload

---
+++ ReverseString.java
@@ -0,0 +1,14 @@
+import java.util.scanner;
+public class ReverseString
+{
+ public static void main(String args[])
+ {
+ scanner a=new scanner(system.in);
+ string s=a.nextLine();
+ int n=s.length();
+ for(int i=(n-1);i.=0;i--)
+{
+system.out.print(s.charAt(i);
+}
+ }
+}
